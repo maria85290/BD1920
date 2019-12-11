54e59e16f9e223e5318204eb695e72ed5dbf5959
Filipe Freitas <filipejsfreitas@gmail.com>
2019-12-11 11:19:31 +0000

    Continuar o script de populacao da BD

--- gerar_atletas_sql.py
+++ gerar_atletas_sql.py
@@ -1,2 +1,35 @@
 #!/bin/python3
+import sys
+import os
+import random
+import numpy.random
 
+
+def read_file(filename):
+    if not os.path.exists(filename):
+        exit(-1)
+
+    with open(filename) as file:
+        return file.readlines()
+
+
+if __name__ == "__main__":
+    atletas = read_file('atletas.txt')
+    medicos = read_file('medicos.txt')
+    modalidades = read_file('modalidades.txt')
+    tipos_testes = read_file('testes.txt')
+
+    random.seed(0)
+
+    # Cada atleta tem:
+    ## id
+    ## sexo
+    ## nome
+    ## peso
+    ## altura
+    ## morada
+    ## date of birth
+    ## modalidade que pratica
+    for nome_alteta in atletas:
+        sexo = random.choice(['M', 'F'])
+        peso = random.choice()
